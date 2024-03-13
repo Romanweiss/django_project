77e1b6803836d5d6304a87d8c5a741934e15f0df
Roman Weiß <romanweiblearning@gmail.com>
2024-03-13 21:08:06 +0300

    add search

--- TestProject/appKey/urls.py
+++ TestProject/appKey/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    # path('', views.HomeKey.as_view(), name="urlHomeKey"),
-    path('', views.CategorysPage.as_view(), name="urlCategorys"),
-    path('category/<int:id>', views.ProductPage.as_view(), name="urlProduct"),
-    # path('full-product/<int:id>/', views.FullProduct.as_view(), name="urlFullProduct"),
+    # path('', views.HomeKey.as_view(), name='urlHomeKey'),
+    path('', views.CategorysPage.as_view(), name='urlCategorys'),
+    path('category/<int:id>', views.ProductPage.as_view(), name='urlProduct'),
+    path('search', views.Search.as_view(), name='urlSearch')
+    # path('full-product/<int:id>/', views.FullProduct.as_view(), name='urlFullProduct'),
 ]
